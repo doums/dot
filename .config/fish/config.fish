fca8fea6c6e4ab39386b9f83f814633ea69d45ed
pierre <dommerc.pierre@gmail.com>
2019-12-22 03:36:58 +0100

    added ripgrep as command to run when using fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,15 +11,21 @@ alias dbk="adb shell am force-stop com.monbuilding.app"
 alias rr="adb shell input text "RR""
 alias ds="xrandr --output eDP-1 --auto --output DP-2 --auto --scale 1.33333x1.33333 --right-of eDP-1"
 
+set fish_greeting
 set -x LD_LIBRARY_PATH /usr/local/lib $LD_LIBRARY_PATH
 set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
 set -x GDK_SCALE 2
 set -x GDK_DPI_SCALE 0.5
 set -x ANDROID_HOME $HOME/Android/Sdk
-set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk
+set -x JAVA_HOME /usr/lib/jvm/default
 set -x XDG_SESSION_TYPE X11
 set -x PATH /opt/arcanist/bin $PATH
 set -x PATH /home/pierre/Android/Sdk/platform-tools $PATH
 set -x PATH $HOME/.cargo/env $PATH
 set -x fish_emoji_width 2
+set -x PATH /opt/node/bin $PATH
+set -x PATH /home/pierre/.yarn/bin $PATH
 set -x MANPAGER "vim -M +MANPAGER -"
+set -x MAKEFLAGS "-j8"
+set -x BROWSER none
+set -x FZF_DEFAULT_COMMAND "rg --files --hidden --no-ignore --follow"
