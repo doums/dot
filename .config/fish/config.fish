f1eb45d3b769e7daa67dbeb29b05e77a2453f3dd
pierre <dommerc.pierre@gmail.com>
2020-05-11 01:40:13 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,13 +8,14 @@ alias ll='ls -l --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color
 alias la='ls -la --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias grep='grep --color=tty -d skip'
 alias sb='subl'
-alias db="adb shell input keyevent 82"
-alias dbk="adb shell am force-stop com.monbuilding.app"
-alias rr="adb shell input text "RR""
-alias ds="xrandr --output DP-2 --auto --scale 1.333 --above eDP-1"
-alias fk="fk.sh"
-alias fkp="fkp.sh"
-alias pac="pac.sh"
+alias db='adb shell input keyevent 82'
+alias dbk='adb shell am force-stop com.monbuilding.app'
+alias rr='adb shell input text "RR"'
+alias ds='xrandr --output DP-2 --auto --scale 1.333 --above eDP-1'
+alias fk='fk.sh'
+alias fkp='fkp.sh'
+alias pac='pac.sh'
+alias vim='nvim'
 
 set fish_greeting
 set -x LD_LIBRARY_PATH /usr/local/lib $LD_LIBRARY_PATH
@@ -35,8 +36,8 @@ set -x PATH /opt/node/bin $PATH
 set -x PATH /home/pierre/.yarn/bin $PATH
 set -x PATH $HOME/.local/bin $PATH
 set -x PATH $PATH $HOME/.local/share/JetBrains/Toolbox/bin
-set -x MANPAGER "vim -M +MANPAGER -"
-set -x MAKEFLAGS "-j8"
+set -x MANPAGER 'nvim +Man!'
+set -x MAKEFLAGS '-j8'
 set -x BROWSER none
 set -x FZF_DEFAULT_OPTS "--bind 'ctrl-w:toggle-preview-wrap,ctrl-p:toggle-preview' --color='bg:#2e3440,fg:#eceff4,preview-bg:#2e3440,preview-fg:#eceff4,border:#d8dee9,bg+:#434c5e,fg+:#eceff4,hl:#8fbcbb,hl+:#88c0d0,gutter:#3b4252,pointer:#e5e9f0,prompt:#88c0d0,marker:#81a1c1,spinner:#81a1c1,header:#5e81ac,info:#81a1c1'"
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden --no-ignore"
