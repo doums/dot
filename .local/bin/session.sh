6847385edc2806062b34c79b6027c5db5d37bc4d
pierre <dommerc.pierre@gmail.com>
2020-06-28 17:47:27 +0200

    wip

---
+++ .local/bin/session.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+red="\e[38;5;1m"
+bold="\e[1m"
+reset="\e[0m"
+
+if ! dmenu -v &> /dev/null; then
+  >&2 printf "%bThis script needs %bdemnu%b%b to work.%b\n" "$red" "$bold" "$reset" "$red" "$reset"
+  exit 1
+fi
+
+choice=$(printf "lock\nlogout\nsuspend\npoweroff\nreboot" | dmenu -b -i -p 'session' "$@")
+case $choice in
+  "lock") lock.sh;;
+  "logout") pkill -SIGTERM spectrwm;;
+  "suspend") systemctl suspend;;
+  "poweroff") systemctl poweroff;;
+  "reboot" ) systemctl reboot;;
+esac
