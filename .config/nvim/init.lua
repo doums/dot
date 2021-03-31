f89b1ed7818bf49733fb109289cb7d47bceae2af
pierre <dommerc.pierre@gmail.com>
2021-03-31 23:14:06 +0200

    nvim switch to lua wip

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 local fn = vim.fn
 
 -- PLUGINS -------------------------------------------------------
+
 -- auto install paq-nvim
 local install_path = fn.stdpath('data')..'/site/pack/paqs/opt/paq-nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -14,7 +15,7 @@ paq {'savq/paq-nvim', opt=true}    -- Let Paq manage itself
 -- update treesitter parsers
 local function update_ts_parsers() vim.cmd('TSUpdate') end
 
-paq 'scrooloose/nerdcommenter'
+paq 'b3nj5m1n/kommentary'
 paq 'airblade/vim-gitgutter'
 paq 'dense-analysis/ale'
 paq 'doums/barow'
@@ -32,16 +33,33 @@ paq {'nvim-treesitter/nvim-treesitter', run=update_ts_parsers}
 paq 'nvim-treesitter/playground'
 ------------------------------------------------------------------
 
--- helpers
-local function opt(key, value, ...)
+-- HELPERS -------------------------------------------------------
+
+--[[ make buffer and window option global as well
+     TODO use vim.opt when released
+     see https://github.com/neovim/neovim/pull/13479 ]]
+local function opt(key, value, scope)
+  scope = scope or 'o'
   local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
-  local scope = select(1, ...)
-  if not scope then scope = 'o' end
   scopes[scope][key] = value
   if scope ~= 'o' then scopes['o'][key] = value end
 end
 
+-- `t` for `termcodes`.
+local function t(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+-- use `nvim_set_keymap` with `noremap` option set to `true` by default
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {noremap = true}
+  if opts.noremap == nil then opts.noremap = true end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
+------------------------------------------------------------------
+
 -- OPTIONS -------------------------------------------------------
+
 opt('termguicolors', true)
 opt('number', true, 'w')
 opt('relativenumber', true, 'w')
@@ -78,3 +96,22 @@ vim.cmd 'colorscheme darcula'
 -- nvim as man pager
 vim.cmd 'runtime ftplugin/man.vim'
 
+-- map leader
+vim.g.mapleader = ','
+
+-- MAPPINGS ------------------------------------------------------
+
+-- c'est en forgeant que l'on devient forgeron
+map('', '<Up>', '<Nop>')
+map('', '<Down>', '<Nop>')
+map('', '<Right>', '<Nop>')
+map('', '<Left>', '<Nop>')
+------------------------------------------------------------------
+
+-- kommentary ----------------------------------------------------
+
+vim.g.kommentary_create_default_mappings = false
+vim.api.nvim_set_keymap("n", "<leader>cc", "<Plug>kommentary_line_default", {})
+vim.api.nvim_set_keymap("n", "<leader>c", "<Plug>kommentary_motion_default", {})
+vim.api.nvim_set_keymap("v", "<leader>c", "<Plug>kommentary_visual_default", {})
+------------------------------------------------------------------
