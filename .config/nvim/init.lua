5c355ee44d28c5f0dbfd9ad6913df77039cec117
pierre <dommerc.pierre@gmail.com>
2021-03-31 10:21:03 +0200

    wip

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
--- auto install paq-nvim
-local execute = vim.api.nvim_command
 local fn = vim.fn
+
+-- PLUGINS -------------------------------------------------------
+-- auto install paq-nvim
 local install_path = fn.stdpath('data')..'/site/pack/paqs/opt/paq-nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd('!git clone https://github.com/savq/paq-nvim.git '..install_path)
@@ -11,9 +12,8 @@ local paq = require'paq-nvim'.paq  -- Import module and bind `paq` function
 paq {'savq/paq-nvim', opt=true}    -- Let Paq manage itself
 
 -- update treesitter parsers
-local function updateTSParsers() vim.cmd('TSUpdate') end
+local function update_ts_parsers() vim.cmd('TSUpdate') end
 
--- plugins
 paq 'scrooloose/nerdcommenter'
 paq 'airblade/vim-gitgutter'
 paq 'dense-analysis/ale'
@@ -28,11 +28,49 @@ paq 'doums/barowLSP'
 paq 'doums/barowGit'
 paq 'doums/rgv'
 paq {'neoclide/coc.nvim', branch='release'}
-paq {'nvim-treesitter/nvim-treesitter', run=updateTSParsers}
+paq {'nvim-treesitter/nvim-treesitter', run=update_ts_parsers}
 paq 'nvim-treesitter/playground'
+------------------------------------------------------------------
 
--- options
+-- helpers
+local function opt(key, value, ...)
+  local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+  local scope = select(1, ...)
+  if not scope then scope = 'o' end
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
 
+-- OPTIONS -------------------------------------------------------
+opt('termguicolors', true)
+opt('number', true, 'w')
+opt('relativenumber', true, 'w')
+opt('showmode', false)
+opt('shortmess', 'IFaWcs')
+opt('ignorecase', true)
+opt('smartcase', true)
+opt('cindent', true, 'b')
+opt('tabstop', 2, 'b')
+opt('shiftwidth', 2, 'b')
+opt('expandtab', true, 'b')
+opt('showmatch', true)
+opt('matchtime', 3)
+opt('updatetime', 100)
+opt('splitbelow', true)
+opt('splitbelow', true)
+opt('splitright', true)
+opt('foldlevelstart', 0)
+opt('hidden', true)
+opt('cursorline', true, 'w')
+opt('switchbuf', 'usetab')
+opt('scrolloff', 5, 'w')
+opt('completeopt', 'menuone')
+opt('pumheight', 10)
+opt('fillchars', 'vert: ,diff: ,fold: ', 'w')
+opt('complete', vim.bo.complete..',i', 'b')
+opt('clipboard', 'unnamedplus')
+opt('guicursor', '')
+------------------------------------------------------------------
 
 -- color scheme
 vim.cmd 'colorscheme darcula'
