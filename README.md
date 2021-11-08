ed94c3f96b6d177aa2d3c3af113b09a20cf1e6a4
pierre <dommerc.pierre@gmail.com>
2021-11-08 11:09:29 +0100

    switch to XMonad

--- README.md
+++ README.md
@@ -3,44 +3,56 @@
 ## ArchLinux installation
 
 Linux build:
+
 - display server: Xorg (X11)
 - no desktop environment
 - display manager: LightDM + its gtk greeter
-- window managers: spectrwm & dwm
+- window manager: XMonad
 - compositor: picom
 
 ### in the live environment
+
 Connect in wireless using `iwd`
-```
+
+```shell
 $ iwctl
 ```
+
 resources: https://wiki.archlinux.org/index.php/Iwd#Connect_to_a_network
 
 ### during the arch-chroot session (aka Inception)
+
 Install `base-devel` and `networkmanager` packages
-```
+
+```shell
 pacman -S base-devel networkmanager
 ```
 
 ### first boot in the fresh installed Linux
+
 start the service `NetworkManager.service`
-```
+
+```shell
 sudo systemctl enable NetworkManager.service
 sudo systemctl start NetworkManager.service
 ```
+
 then connect to a network using `nmtui`
 
 source: https://wiki.archlinux.org/index.php/NetworkManager#Installation
 
 ### enable periodic TRIM
-```
+
+```shell
 sudo systemctl enable fstrim.timer
 sudo systemctl start fstrim.timer
 ```
+
 source: https://wiki.archlinux.org/index.php/Solid_state_drive#Periodic_TRIM
 
 ### create XDG user directories
-```
+
+```shell
 sudo pacman -S xdg-user-dirs
 xdg-user-dirs-update
 ```
@@ -50,50 +62,123 @@ source: https://wiki.archlinux.org/index.php/XDG_user_directories
 ### graphical environment setup
 
 1. install the display server Xorg (implementation of the X Window System aka X11) and the display driver (`mesa` for intel integrated graphic card)
-```
+
+```shell
 sudo pacman -S xorg-server mesa
 ```
+
 https://wiki.archlinux.org/index.php/Xorg\
 https://wiki.archlinux.org/index.php/Intel_graphics#Installation
 
 2. install the display manager LightDM and its greeter
-```
+
+```shell
 sudo pacman -S lightdm lightdm-gtk-greeter
 ```
+
 start the service
-```
+
+```shell
 sudo systemctl enable lightdm
 ```
+
 https://wiki.archlinux.org/index.php/LightDM#Installation
 
-3. install window managers
-#### spectrwm
-```
-rua install spectrwm-git
-```
-source: https://github.com/conformal/spectrwm
+3. install the window manager
 
-#### dwm
-see https://github.com/doums/dwm
+#### XMonad
+
+First install `stack`. Take the static version from AUR to avoid
+the plethora of Haskell dependencies.
+
+```shell
+rua install stack-static
+```
+
+Chose a location to clone the sources and build the project
+
+```shell
+mkdir Documents/xmonad
+cd Documents/xmonad
+git clone https://github.com/xmonad/xmonad
+git clone https://github.com/xmonad/xmonad-contrib
+```
+
+Add the configuration file from `xmonad/xmonad.hs` and init the
+project to generate the `stack.yaml` file
+
+```shell
+stack init
+```
+
+Then build and install it
+
+```shell
+stack install
+```
+
+This will install `xmonad` binary under `~/.local/bin`.
+
+`xmonad --recompile` expects to find the project configuration and
+the XMonad config in `$XDG_CONFIG_HOME/xmonad`. To avoid to move
+the whole project under `$XDG_CONFIG_HOME/xmonad` (which doesn't
+make sense) symlink these files.
+
+```shell
+mkdir ~/.config/xmonad
+cd ~/.config/xmonad
+ln -s ~/Documents/xmonad/xmonad.hs xmonad.hs
+ln -s ~/Documents/xmonad/stack.yaml stack.yaml
+```
+
+source: https://xmonad.org/INSTALL.html
+
+##### XMobar
+
+Clone the sources and build the project
+
+```shell
+git clone git://github.com/jaor/xmobar
+```
+
+Override `stack.yaml` to only build needed features
+
+```shell
+cp xmobar/stack.yaml .
+```
+
+Add `xmobarrc` to `~/config/xmobar/`.
+
+Finally build and install it
+
+```shell
+stack install
+```
+
+source:https://xmobar.org/#from-source
 
 ### mount helper and USB
-```
+
+```shell
 sudo pacman -S udisks2 udiskie
 ```
-source: https://wiki.archlinux.org/index.php/Udisks
 
+source: https://wiki.archlinux.org/index.php/Udisks
 
 ## Design porn
 
 ### gtk theme, Adapta
+
 https://github.com/adapta-project/adapta-gtk-theme.git
 
 (see notes below for how to build it for HiDPI)
 
 ### icon and cursor theme
+
 install https://github.com/snwh/paper-icon-theme
 
 edit `/usr/share/icons/default/index.theme`
+
 ```
 [Icon Theme]
 Inherits=Paper
@@ -102,18 +187,23 @@ Inherits=Paper
 note: the icon theme is also set when `.Xresources` file is read when lightdm (display manager) starts and sources `.xprofile`
 
 resources:
+
 - https://wiki.archlinux.org/index.php/Icons#Manually
 - https://wiki.archlinux.org/index.php/Cursor_themes#XDG_specification
 
 ### set gtk theme
+
 in `$XDG_CONFIG_HOME/gtk-3.0/settings.ini`
+
 ```
 [Settings]
 gtk-icon-theme-name = Paper
 gtk-theme-name = Adapta-Nokto
 gtk-font-name = Roboto 12
 ```
+
 in ~/.gtkrc-2.0
+
 ```
 gtk-icon-theme-name = "Paper"
 gtk-theme-name = "Adapta-Nokto"
@@ -123,18 +213,22 @@ gtk-font-name = "Roboto 12"
 ### fonts
 
 first install some TTF fonts
-```
+
+```shell
 sudo pacman -S noto-fonts ttf-dejavu ttf-liberation
 ```
+
 put the files located in the `font` directory in `/usr/share/fonts/TTF` and make them readable by every user
 
 update de font cache
-```
+
+```shell
 fc-cache
 ```
 
 **emoji support**
-```
+
+```shell
 $ sudo pacman -S noto-fonts-emoji
 ```
 
@@ -143,41 +237,54 @@ $ sudo pacman -S noto-fonts-emoji
 Material Design Icons font: https://github.com/Templarian/MaterialDesign-Font
 
 font patcher: https://github.com/ryanoasis/nerd-fonts#font-patcher
+
 1. install FontForge
-```
+
+```shell
 sudo pacman -S FontForge
 ```
+
 2. clone the repo
-```
+
+```shell
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ```
+
 3. move the MDI font in `src/glyphs/`
 4. patch
-```
+
+```shell
 ./font-patcher --custom MaterialDesignIconsDesktop.ttf --progressbars --careful path/to/fontToPatch.ttf
 ```
 
 ### HiDPI
 
 DPI is set in `.Xresources`
+
 ```
 Xft.dpi: 144
 ```
+
 it will be loaded during display manager (ligthdm) starts and sources `.xprofile`
-```
+
+```sh
 [[ -f "$home/.Xresources" ]] && xrdb -merge $home/.Xresources
 ```
 
 sources:
+
 - https://wiki.archlinux.org/index.php/HiDPI#X_Resources
 - https://wiki.archlinux.org/index.php/LightDM#Environment_variables
 
 larger font for linux console
-```
+
+```shell
 $ sudo pacman -S terminus-font
 ```
+
 add in `/etc/vconsole.conf`
+
 ```
 FONT=ter-v22b
 FONT_MAP=cp437
@@ -185,33 +292,40 @@ FONT_MAP=cp437
 
 source: https://wiki.archlinux.org/index.php/HiDPI#Linux_console
 
-
 ## notes
 
 The following notes are relevant to the installation of ArchLinux.
 
-
 ### solve icon problem for apps installed through Flatpak
-```
+
+```shell
 cd /var/lib/flatpak/exports/share/applications
 ```
+
 rename the links to match this format: obs.desktop, vlc.desktop
 
 ### journal
+
 To prevent the journal to take 4Gb space of disk memory in `/etc/systemd/journald.conf` add :
+
 ```
 SystemMaxUse=1G
 ```
+
 (the default is 4Gb)
 
 source: https://wiki.archlinux.org/index.php/Systemd/Journal#Journal_size_limit
 
 ### fix Adapta theme for HiDPI
+
 install build dependencies
-```
+
+```shell
 sudo pacman -S inkscape sassc parallel
 ```
+
 in `wm/asset/assets-xfwm-scripts/render-assets-xfwm.sh` make these changes
+
 ```
  if [ "$inkver" = 0.91 ]; then
 -    non_scale_dpi=90
@@ -221,20 +335,24 @@ in `wm/asset/assets-xfwm-scripts/render-assets-xfwm.sh` make these changes
 +    non_scale_dpi=144
  fi
 ```
+
 then build and install:
-```
+
+```shell
 ./autogen.sh --prefix=/usr --disable-cinnamon --disable-mate --disable-gnome --disable-flashback --disable-openbox --enable-parallel
 make
 sudo make install
 ```
+
 remove build dependencies
-```
+
+```shell
 sudo pacman -Rsn inkscape sassc parallel
 ```
 
 ### gnome keyring
 
-```
+```shell
 sudo pacman -S gnome-keyring
 ```
 
@@ -243,11 +361,14 @@ https://wiki.archlinux.org/index.php/GNOME/Keyring#Installation
 ### lower GRUB resolution
 
 In `/etc/default/grub` change the line (replace with the desired resolutions)
+
 ```
 GRUB_GFXMODE=1600x1200x32,2560x1440x32,auto
 ```
+
 then re-generate the config
-```
+
+```shell
 grub-mkconfig -o /boot/grub/grub.cfg
 ```
 
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,348 @@
+--
+-- xmonad example config file.
+--
+-- A template showing all available configuration hooks,
+-- and how to override the defaults in your own xmonad.hs conf file.
+--
+-- Normally, you'd only override those defaults you care about.
+--
+
+-- Base
+import XMonad
+import System.Exit
+import qualified XMonad.StackSet as W
+
+-- Data
+import Data.Monoid
+import qualified Data.Map as M
+
+-- Actions
+import XMonad.Actions.CycleWS
+import XMonad.Actions.WithAll (killAll)
+
+-- Layouts
+import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed
+
+-- Hooks
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.InsertPosition
+
+-- Utils
+import XMonad.Util.Cursor
+import XMonad.Util.SpawnOnce
+
+-- XF86 keys
+import Graphics.X11.ExtraTypes.XF86
+
+-- The preferred terminal program, which is used in a binding below and by
+-- certain contrib modules.
+--
+myTerminal = "alacritty"
+
+-- Whether focus follows the mouse pointer.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+-- Whether clicking on a window to focus also passes the click to the window
+myClickJustFocuses :: Bool
+myClickJustFocuses = False
+
+-- Width of the window border in pixels.
+--
+myBorderWidth = 4
+
+-- Amount of space around windows
+--
+mySpacing :: Int
+mySpacing = 7
+
+-- modMask lets you specify which modkey you want to use. The default
+-- is mod1Mask ("left alt").  You may also consider using mod3Mask
+-- ("right alt"), which does not conflict with emacs keybindings. The
+-- "windows key" is usually mod4Mask.
+--
+myModMask = mod4Mask
+
+-- The default number of workspaces (virtual screens) and their names.
+-- By default we use numeric strings, but any string may be used as a
+-- workspace name. The number of workspaces is determined by the length
+-- of this list.
+--
+-- A tagging example:
+--
+-- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
+--
+myWorkspaces    = ["1", "2", "3", "a", "z", "e", "4", "r", "5"]
+myWorkspaceKeys = [ xK_ampersand
+                  , xK_eacute
+                  , xK_quotedbl
+                  , xK_a
+                  , xK_z
+                  , xK_e
+                  , xK_apostrophe
+                  , xK_r
+                  , xK_parenleft ]
+
+-- colors
+--
+grey = "#404040"
+darkGrey = "#262626"
+nearBlack = "#0d0d0d"
+stone = "#8c8c8c"
+
+-- dmenu args
+--
+dmenuFn = "'JetBrains Mono:pixelsize=22:antialias=true'"
+dDarkGrey = "\\#262626"
+dNearBlack = "\\#0d0d0d"
+dStone = "\\#8c8c8c"
+
+dmenuArgs :: String
+dmenuArgs = " -fn " ++ dmenuFn ++ " -nb " ++ dNearBlack ++ " -nf " ++ dStone ++ " -sb " ++ dDarkGrey ++ " -sf " ++ dStone
+
+-- toggle xmobar command
+--
+toggleXmobar :: String
+toggleXmobar = "dbus-send --session --dest=org.Xmobar.Control --type=method_call '/org/Xmobar/Control' org.Xmobar.Control.SendSignal 'string:Toggle 0'"
+
+------------------------------------------------------------------------
+-- Key bindings. Add, modify or remove key bindings here.
+--
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    [ ((modm,                 xK_t),                     spawn $ XMonad.terminal conf)
+    , ((modm,                 xK_exclam),                spawn "rofi -show drun")
+    , ((modm,                 xK_w),                     spawn "rofi -show window")
+    , ((modm .|. shiftMask,   xK_l),                     spawn "lock.sh")
+    , ((modm,                 xK_q),                     spawn ("session.sh" ++ dmenuArgs))
+    , ((modm .|. controlMask, xK_q),                     spawn "xmonad --recompile; xmonad --restart")
+    , ((modm,                 xK_d),                     spawn ("set_dp.sh" ++ dmenuArgs))
+    , ((0,                    xF86XK_MonBrightnessUp),   spawn "pral.sh light_up")
+    , ((0,                    xF86XK_MonBrightnessDown), spawn "pral.sh light_down")
+    , ((0,                    xF86XK_AudioRaiseVolume),  spawn "pral.sh sink_up")
+    , ((0,                    xF86XK_AudioLowerVolume),  spawn "pral.sh sink_down")
+    , ((0,                    xF86XK_AudioMute),         spawn "pral.sh sink_mute")
+    , ((0,                    xF86XK_AudioMicMute),      spawn "pral.sh source_mute")
+    , ((modm,                 xK_asterisk),              spawn "pkill -USR1 redshift")
+    , ((0,                    xK_Print),                 spawn "screenshot.sh")
+    , ((modm,                 xK_c),                     spawn "clipshot.sh")
+    , ((modm,                 xK_x),                     kill)
+    , ((modm .|. shiftMask,   xK_x),                     killAll)
+    , ((modm,                 xK_space),                 sendMessage NextLayout)
+    , ((modm .|. controlMask, xK_r),                     refresh)
+    , ((modm,                 xK_j),                     windows W.focusDown)
+    , ((modm,                 xK_k),                     windows W.focusUp)
+    , ((modm,                 xK_m),                     windows W.focusMaster)
+    , ((modm,                 xK_Return),                windows W.swapMaster)
+    , ((modm .|. shiftMask,   xK_j),                     windows W.swapDown)
+    , ((modm .|. shiftMask,   xK_k),                     windows W.swapUp)
+    , ((modm,                 xK_f),                     withFocused $ windows . W.sink)
+    , ((modm,                 xK_Left),                  prevWS)
+    , ((modm,                 xK_Right),                 nextWS)
+    , ((modm .|. shiftMask,   xK_Left),                  shiftToPrev)
+    , ((modm .|. shiftMask,   xK_Right),                 shiftToNext)
+    , ((modm,                 xK_h),                     sendMessage Shrink)
+    , ((modm,                 xK_l),                     sendMessage Expand)
+    , ((modm,                 xK_semicolon),             sendMessage (IncMasterN 1))
+    , ((modm,                 xK_comma),                 sendMessage (IncMasterN (-1)))
+    , ((modm,                 xK_b),                     spawn toggleXmobar)
+    ]
+    ++
+
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) myWorkspaceKeys
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    -- ++
+
+    --
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    --
+    -- [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        -- | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        -- , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+------------------------------------------------------------------------
+-- Mouse bindings: default actions bound to mouse events
+--
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    -- mod-button1, Set the window to floating mode and move by dragging
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
+                                       >> windows W.shiftMaster))
+
+    -- mod-button2, Raise the window to the top of the stack
+    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+
+    -- mod-button3, Set the window to floating mode and resize by dragging
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster))
+
+    -- you may also bind events to the mouse scroll wheel (button4 and button5)
+    ]
+
+------------------------------------------------------------------------
+-- Layouts:
+
+-- You can specify and transform your layouts by modifying these values.
+-- If you change layout bindings be sure to use 'mod-shift-space' after
+-- restarting (with 'mod-q') to reset your layout state to the new
+-- defaults, as xmonad preserves your old layout settings by default.
+--
+-- The available layouts.  Note that each layout is separated by |||,
+-- which denotes layout choice.
+--
+myLayout = smartBorders $ avoidStruts $ spacingWithEdge mySpacing $ tiled ||| Mirror tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+------------------------------------------------------------------------
+-- Window rules:
+
+-- Execute arbitrary actions and WindowSet manipulations when managing
+-- a new window. You can use this to, for example, always float a
+-- particular program, or have a client always appear on a particular
+-- workspace.
+--
+-- To find the property name associated with a program, use
+-- > xprop | grep WM_CLASS
+-- and click on the client you're interested in.
+--
+-- To match on the WM_NAME, you can use 'title' in the same way that
+-- 'className' and 'resource' are used below.
+--
+myManageHook = insertPosition End Newer <+> composeAll
+    [ className =? "Gimp"              --> doFloat
+    , className =? "jetbrains-toolbox" --> doIgnore
+    , className =? "feh"               --> doFloat
+    , className =? "Galculator"        --> doFloat
+    , className =? "TeamSpeak 3"       --> doFloat
+    , resource  =? "desktop_window"    --> doIgnore ]
+
+------------------------------------------------------------------------
+-- Event handling
+
+-- * EwmhDesktops users should change this to ewmhDesktopsEventHook
+--
+-- Defines a custom handler function for X Events. The function should
+-- return (All True) if the default handler is to be run afterwards. To
+-- combine event hooks use mappend or mconcat from Data.Monoid.
+--
+myEventHook = mempty
+
+------------------------------------------------------------------------
+-- Status bars and logging
+
+-- Perform an arbitrary action on each internal state change or X event.
+-- See the 'XMonad.Hooks.DynamicLog' extension for examples.
+--
+myLogHook = return ()
+
+------------------------------------------------------------------------
+-- Startup hook
+
+-- Perform an arbitrary action each time xmonad starts or is restarted
+-- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
+-- per-workspace layout choices.
+--
+-- By default, do nothing.
+myStartupHook = do
+    setDefaultCursor xC_left_ptr -- set default cursor
+    spawnOnce "picom --config /home/pierre/.config/picom.conf -b"
+    spawnOnce "gnome-keyring-daemon --start -d"
+    spawnOnce "redshift -c /home/pierre/.config/redshift/redshift.conf"
+    spawnOnce "dunst -c /home/pierre/.config/dunst/dunstrc"
+    spawnOnce "udiskie"
+    spawnOnce "bato"
+
+------------------------------------------------------------------------
+-- xmobarPP
+--
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep              = "  "
+    , ppWsSep            = " "
+    , ppCurrent          = stone . wrap (white "❱") ""
+    , ppVisible          = stone . wrap "❯" ""
+    , ppHidden           = stone . wrap (white "•") ""
+    , ppHiddenNoWindows  = stone . wrap " " ""
+    , ppVisibleNoWindows = Just $  stone . wrap "❯" ""
+    , ppUrgent           = stone . wrap (red "⚡") ""
+    , ppOrder            = \[ws, l, w] -> [ws, l] -- \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras           = [] -- [logTitles formatFocused formatUnfocused]
+    }
+  where
+    -- formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    -- formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    -- ppWindow :: String -> String
+    -- ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    stone, red, white :: String -> String
+    white    = xmobarColor "#ffffff" ""
+    red      = xmobarColor "#bf616a" ""
+    stone    = xmobarColor "#8c8c8c" ""
+
+------------------------------------------------------------------------
+-- Now run xmonad with all the defaults we set up.
+
+-- Run xmonad with the settings you specify. No need to modify this.
+--
+main :: IO ()
+main = xmonad
+     . docks
+     . ewmhFullscreen
+     . ewmh
+     . withSB (statusBarProp "xmobar" (pure myXmobarPP))
+     $ myConfig
+
+-- A structure containing your configuration settings, overriding
+-- fields in the default config. Any you don't override, will
+-- use the defaults defined in xmonad/XMonad/Config.hs
+--
+-- No need to modify this.
+--
+myConfig = def {
+      -- simple stuff
+        terminal           = myTerminal,
+        focusFollowsMouse  = myFocusFollowsMouse,
+        clickJustFocuses   = myClickJustFocuses,
+        borderWidth        = myBorderWidth,
+        modMask            = myModMask,
+        workspaces         = myWorkspaces,
+        normalBorderColor  = darkGrey,
+        focusedBorderColor = grey,
+
+      -- key bindings
+        keys               = myKeys,
+        mouseBindings      = myMouseBindings,
+
+      -- hooks, layouts
+        layoutHook         = myLayout,
+        manageHook         = myManageHook,
+        handleEventHook    = myEventHook,
+        logHook            = myLogHook,
+        startupHook        = myStartupHook
+    }
+
