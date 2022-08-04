b9294dd5333f258d4ab54c160ab422fbc1e0601c
pierre <dommerc.pierre@gmail.com>
2022-08-04 17:00:19 +0200

    nnn: wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,11 +26,6 @@ set -x EDITOR /usr/bin/nvim
 set -x VISUAL /usr/bin/nvim
 set -x GDK_SCALE 2
 set -x GDK_DPI_SCALE 0.5
-set -x ANDROID_HOME $HOME/Android/Sdk
-set -x PATH $ANDROID_HOME/emulator $PATH
-set -x PATH $ANDROID_HOME/tools $PATH
-set -x PATH $ANDROID_HOME/tools/bin $PATH
-set -x PATH $ANDROID_HOME/platform-tools $PATH
 set -x JAVA_HOME /usr/lib/jvm/default
 set -x XDG_SESSION_TYPE X11
 set -x PATH $PATH $HOME/.cargo/bin
@@ -39,14 +34,14 @@ set -x fish_emoji_width 2
 set -x PATH /opt/ltex-ls/bin $PATH
 set -x PATH /home/pierre/.yarn/bin $PATH
 set -x PATH $HOME/.local/bin $PATH
-set -x PATH $HOME/.local/jetbrains $PATH
+set -x PATH $PATH $HOME/.local/share/JetBrains/Toolbox/scripts
 set -x PATH $PATH $HOME/.local/share/JetBrains/Toolbox/bin
 set -x MANPAGER 'nvim +Man!'
 set -x MAKEFLAGS '-j8'
 set -x FZF_DEFAULT_OPTS "--bind 'ctrl-w:toggle-preview-wrap,ctrl-p:toggle-preview,ctrl-a:toggle-all' --color='bg:-1,fg:-1,preview-bg:-1,preview-fg:-1,border:black:bold,bg+:#323232,fg+:-1:regular,hl:magenta:bold,hl+:magenta:bold,gutter:-1,pointer:yellow,prompt:blue,marker:green,spinner:bright-cyan,info:cyan:italic,header:blue:bold,query:-1:bold' --pointer=❱ --marker=❯ --prompt='❯ '"
 set -x FZF_DEFAULT_COMMAND "fd -HI -t f"
 set -x XDG_DATA_DIRS /home/pierre/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-set -x NNN_OPTS QAex
+set -x NNN_OPTS QADuex
 set -x NNN_COLORS 2341
 set -x NNN_PLUG 't:-_bat $nnn;f:-_|firefox $nnn*'
 set -x NNN_BMS 'h:~;d:~/Documents;r:/;m:/run/media/pierre'
