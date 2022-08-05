18deb710844e7ba93229259b2d6fd5bfe7ac3126
pierre <dommerc.pierre@gmail.com>
2022-08-05 18:54:17 +0200

    xmonad: switch to topics and scratchpads

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,11 +2,12 @@ import XMonad
 import XMonad.Util.EZConfig
 import System.Exit
 import qualified XMonad.StackSet as W
-import Data.Monoid
 import qualified Data.Map as M
+import Data.Monoid
 import XMonad.Actions.CycleWS
 import XMonad.Actions.CycleRecentWS
 import XMonad.Actions.Navigation2D
+import XMonad.Actions.TopicSpace
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Renamed
@@ -17,8 +18,10 @@ import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.WorkspaceHistory
 import XMonad.Util.Cursor
 import XMonad.Util.SpawnOnce
+import XMonad.Util.NamedScratchpad
 import qualified XMonad.Util.Hacks as Hacks
 import Graphics.X11.ExtraTypes.XF86
 
@@ -38,7 +41,7 @@ cfg = def {
   clickJustFocuses   = False,
   borderWidth        = 4,
   modMask            = modm,
-  workspaces         = myWorkspaces,
+  workspaces         = topicNames topicItems,
   normalBorderColor  = darkGrey,
   focusedBorderColor = grey,
   layoutHook         = myLayout,
@@ -76,6 +79,11 @@ cfg = def {
     ,("M-l",         sendMessage Expand)
     ,("M-;",         sendMessage (IncMasterN 1))
     ,("M-,",         sendMessage (IncMasterN (-1)))
+    ,("M-S-<Space>", switchToLast)
+    ,("M-n",         namedScratchpadAction scratchpads "gtrans")
+    ,("M-o",         namedScratchpadAction scratchpads "pavucontrol")
+    ,("M-b",         namedScratchpadAction scratchpads "filemanager")
+    ,("M-=",         namedScratchpadAction scratchpads "calc")
     ,("M-<Page_Down>",   prevWS)
     ,("M-<Page_Up>",     nextWS)
     ,("M-S-<Page_Down>", shiftToPrev)
@@ -88,8 +96,8 @@ cfg = def {
     ,("<XF86AudioMicMute>",      spawn "pral.sh source_mute")]
     ++
     [(m ++ "M-" ++ [k], f i)
-        | (i, k) <- zip myWorkspaces workspaceKeys
-        , (f, m) <- [(windows . W.greedyView, ""), (windows . W.shift, "S-")]]
+        | (i, k) <- zip (topicNames topicItems) topicKeys
+        , (f, m) <- [(goto, ""), (windows . W.shift, "S-")]]
     ++
     [(m ++ "M-" ++ [k], screenWorkspace sc >>= flip whenJust f)
         | (k, sc) <- zip screenKeys [0..]
@@ -108,8 +116,7 @@ nav2D = navigation2DP def ("<Up>", "<Left>", "<Down>", "<Right>")
 
 modm = mod4Mask
 myTerminal = "alacritty"
-myWorkspaces = ["1", "2", "3", "a", "z", "e", "4", "r", "5"]
-workspaceKeys = "&é\"aze'r%"
+topicKeys = "&é\"aze'r%"
 screenKeys = "[]"
 
 grey = "#404040"
@@ -138,20 +145,24 @@ myLayout = renamed [CutWordsLeft 1]
      delta   = 3/100 -- Percent of screen to increment by when resizing panes
 
 myManageHook = fmap not willFloat --> insertPosition Below Newer
+               <+> namedScratchpadManageHook scratchpads
                <+> composeAll
     [ className =? "Gimp"               --> doFloat
-    , className =? "jetbrains-toolbox"  --> doCenterFloat
     , className =? "feh"                --> doFloat
     , className =? "Galculator"         --> doCenterFloat
     , className =? "TeamSpeak 3"        --> doCenterFloat
     , className =? "Insomnia"           --> doCenterFloat
-    , title     =? "win0"               --> doCenterFloat
+    , className =? "jetbrains-toolbox"  --> doCenterFloat
+    , title =? "splash"
+        <&&> className ^? "jetbrains-"  --> doCenterFloat <> hasBorder False
     , title     =? "Contrôle du volume" --> doCenterFloat
     , resource  =? "desktop_window"     --> doIgnore ]
 
-myEventHook = Hacks.trayerAboveXmobarEventHook <> Hacks.windowedFullscreenFixEventHook
+myEventHook = handleEventHook def
+                <> Hacks.trayerAboveXmobarEventHook
+                <> Hacks.windowedFullscreenFixEventHook
 
-myLogHook = return ()
+myLogHook = workspaceHistoryHook
 
 myStartupHook = do
     setDefaultCursor xC_left_ptr -- set default cursor
@@ -161,8 +172,9 @@ myStartupHook = do
     spawnOnce "udiskie"
     -- spawnOnce "bato"
     spawnOnce "clipmenud"
-    spawnOnce "trayer -l --align left --distancefrom left --distance 420 --monitor primary --widthtype request --height 28 --transparent true --alpha 0 --tint 0x262626 --expand true --iconspacing 4 --SetPartialStrut true --SetDockType true"
+    spawnOnce "trayer -l --align left --distancefrom left --distance 540 --monitor primary --widthtype request --height 28 --transparent true --alpha 0 --tint 0x262626 --expand true --iconspacing 4 --SetPartialStrut true --SetDockType true"
 
+-- xmobar
 bar :: PP
 bar = def
     { ppSep              = "  "
@@ -183,3 +195,52 @@ bar = def
     red      = xmobarColor "#bf616a" ""
     stone    = xmobarColor "#8c8c8c" ""
 
+-- topics
+topicItems :: [TopicItem]
+topicItems =
+  [ inHome   "1"                  spawnShell
+  , inHome   "2"                  (spawn "firefox")
+  , TI       "3"    "Documents"   (spawn "jetbrains-toolbox")
+  , inHome   "a"                  (spawn "run_keybase")
+  , noAction "z"    "Documents"
+  , inHome   "e"                  (spawn "flatpak run im.riot.Riot")
+  , noAction "4"      "~"
+  , noAction "r"      "~"
+  , noAction "5"      "~"
+  ]
+
+myTopicConfig :: TopicConfig
+myTopicConfig = def
+  { topicDirs          = tiDirs    topicItems
+  , topicActions       = tiActions topicItems
+  , defaultTopicAction = const (pure ())
+  , defaultTopic       = "1"
+  }
+
+-- topics helper functions
+spawnShell :: X ()
+spawnShell = currentTopicDir myTopicConfig >>= spawnShellIn
+
+spawnShellIn :: Dir -> X ()
+spawnShellIn dir = spawn $ "alacritty --working-directory " ++ dir
+
+goto :: Topic -> X ()
+goto = switchTopic myTopicConfig
+
+switchToLast :: X ()
+switchToLast = switchNthLastFocusedByScreen myTopicConfig 1
+
+-- scratchPads
+scratchpads =
+  [ NS "gtrans" "alacritty --class gtrans -e trans -I -show-original no -show-languages no en:fr"
+      (appName =? "gtrans")
+      (customFloating $ W.RationalRect (2/8) (1/6) (2/8) (4/6))
+  , NS "pavucontrol" "pavucontrol" (className =? "Pavucontrol")
+      (customFloating $ W.RationalRect (1/4) (1/4) (2/4) (2/4))
+  , NS "filemanager" "alacritty --class filemanager -e nnn"
+      (appName =? "filemanager")
+      (customFloating $ W.RationalRect (2/8) (1/6) (3/8) (4/6))
+  , NS "calc" "alacritty --class calc -e kalker"
+      (appName =? "calc")
+      (customFloating $ W.RationalRect (2/8) (2/6) (2/8) (3/6))
+  ]
