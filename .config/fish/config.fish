68bbc0e76d8f352972d1abf6760765c7becfb015
pierre <dommerc.pierre@gmail.com>
2022-10-14 00:21:31 +0200

    env: move env var into .xprofile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,38 +29,10 @@ bind \en '~/Documents/nym/nym/; commandline -f repaint'
 
 # env vars
 set fish_greeting
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x SSH_AUTH_SOCK /run/user/1000/keyring/ssh
-set -x LD_LIBRARY_PATH /usr/local/lib $LD_LIBRARY_PATH
-set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
-set -x EDITOR /usr/bin/nvim
-set -x VISUAL /usr/bin/nvim
-set -x JAVA_HOME /usr/lib/jvm/default
-set -x XDG_SESSION_TYPE X11
-set -x PATH $PATH $HOME/.cargo/bin
-set -x PATH $PATH $HOME/.gem/ruby/2.7.0/bin
 set -x fish_emoji_width 2
-set -x PATH /opt/ltex-ls/bin $PATH
-set -x PATH /home/pierre/.yarn/bin $PATH
-set -x PATH $HOME/.local/bin $PATH
-set -x PATH $PATH $HOME/.local/share/JetBrains/Toolbox/scripts
-set -x PATH $PATH $HOME/.local/share/JetBrains/Toolbox/bin
-set -x MANPAGER 'nvim +Man!'
-set -x MAKEFLAGS '-j8'
-set -x FZF_DEFAULT_OPTS "--bind 'ctrl-w:toggle-preview-wrap,ctrl-p:toggle-preview,ctrl-a:toggle-all,alt-j:preview-half-page-down,alt-k:preview-half-page-up' --color='bg:-1,fg:-1,preview-bg:-1,preview-fg:-1,border:black:bold,bg+:#323232,fg+:-1:regular,hl:magenta:bold,hl+:magenta:bold,gutter:-1,pointer:yellow,prompt:blue,marker:green,spinner:bright-cyan,info:cyan:italic,header:blue:bold,query:-1:bold' --pointer=❱ --marker=❯ --prompt='❯ '"
-set -x FZF_DEFAULT_COMMAND "fd -HI -t f"
-set -x XDG_DATA_DIRS /home/pierre/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-set -x NNN_OPTS QAuex
-set -x NNN_COLORS 2341
-set -x NNN_PLUG 't:-_bat $nnn;f:-_|firefox $nnn*'
-set -x NNN_BMS 'h:~;d:~/Documents/dotfiles;r:/;m:/run/media/pierre;v:~/.config/nvim;n:~/Documents/nym/nym;x:/opt/xmonad'
-set -x NNN_FCOLORS '0505040a00060e0801030301'
-set -x DENO_INSTALL $HOME/.deno
-set -x PATH $PATH $DENO_INSTALL/bin
-set -x LIBVA_DRIVER_NAME vdpau
 set -x GDK_SCALE 2
 set -x GDK_DPI_SCALE 0.5
 
 # Nodejs
-set -x PATH /opt/node16/bin $PATH
+# set -x PATH /opt/node16/bin $PATH
 
