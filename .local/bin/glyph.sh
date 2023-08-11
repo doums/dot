a7f0e4cee4b12bad736dc6e0be2aa39ba2921c37
pierre <dommerc.pierre@gmail.com>
2023-08-11 17:37:26 +0200

    update glyph.sh

--- .local/bin/glyph.sh
+++ .local/bin/glyph.sh
@@ -14,17 +14,18 @@ if ! dmenu -v &> /dev/null; then
   exit 1
 fi
 
-choice=$(printf "✓ check\n✗ error\n⚠ warn\n⚡ power\n→ right\n← left\n↑ up\n↓ down\n✶ star\n⚇ face\n"\
-        | dmenu -b -i -l 10 -p 'glyph' "$@" -fn 'JetBrainsMono:pixelsize=19:antialias=true')
+choice=$(printf "→ right\n✓ check\n✗ error\n⚠ warn\n⚡ power\n← left\n↑ up\n↓ down\n✶ star\n… 3dots\n⚇ face\n"\
+        | dmenu -b -i -l 11 -p 'glyph' "$@" -fn 'JetBrainsMono:pixelsize=19:antialias=true')
 case $choice in
+  "→ right")  echo -n → | xclip -selection clipboard;;
   "✓ check")  echo -n ✓ | xclip -selection clipboard;;
   "✗ error")  echo -n ✗ | xclip -selection clipboard;;
   "⚠ warn")   echo -n ⚠ | xclip -selection clipboard;;
   "⚡ power") echo -n ⚡| xclip -selection clipboard;;
-  "→ right")  echo -n → | xclip -selection clipboard;;
   "← left")   echo -n ← | xclip -selection clipboard;;
   "↑ up")     echo -n ↑ | xclip -selection clipboard;;
   "↓ down")   echo -n ↓ | xclip -selection clipboard;;
   "✶ star")   echo -n ✶ | xclip -selection clipboard;;
+  "… 3dots")  echo -n … | xclip -selection clipboard;;
   "⚇ face")   echo -n ⚇ | xclip -selection clipboard;;
 esac
