00b10f72aabaf0259f5e06ffd877173d5a014960
pierre <dommerc.pierre@gmail.com>
2023-10-25 16:56:21 +0200

    update xmonad cfg

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -235,6 +235,8 @@ myManageHook = fmap not willFloat --> insertPosition Below Newer
     [ className =? "Gimp"               --> doFloat
     , className =? "feh"                --> doFloat
     , className =? "jetbrains-toolbox"  --> doCenterFloat
+    , className =? "frame"              --> doCenterFloat
+    , title =? "frame"                  --> doCenterFloat
     , title =? "splash"
         <&&> className ^? "jetbrains-"  --> doCenterFloat <> hasBorder False
     , title ^? "Emulator"               --> doFloat
@@ -290,14 +292,21 @@ wsActions ws = case ws of
   -- 󱃖
   "\987350" -> spawn $ "jetbrains-toolbox"
   -- 󰭹
-  "\985977" -> spawn "im.riot.Riot"
+  "\985977" -> spawn "im.riot.Riot --profile nym"
   _         -> undefined
+
+-- helper to define terminal commands for scratchpads
+spTerm className cmd = termCmd
+                      ++ className
+                      ++ " -- "
+                      ++ cmd
+  where termCmd = "wezterm --config enable_tab_bar=false start --class "
 
 -- scratchpads
 -- RationalRect `x y width height` - from top left corner, 0-1
 -- 1 means full width/height of the screen
 scratchpads =
-  [ NS "translate" "wezterm start --class translate -- gtt"
+  [ NS "translate" (spTerm "translate" "gtt")
       (appName =? "translate")
       (customFloating $ W.RationalRect 0.25 0.25 0.6 0.4)
   , NS "pavucontrol" "pavucontrol" (className =? "Pavucontrol")
@@ -305,22 +314,22 @@ scratchpads =
   , NS "keymap" "apekey"
       (title =? "apekey")
       (customFloating $ W.RationalRect 0.38 0.12 0.33 0.8)
-  , NS "file-manager" "wezterm start --class file-manager -- nnn"
+  , NS "file-manager" (spTerm "file-manager" "nnn")
       (appName =? "file-manager")
       (customFloating $ W.RationalRect (1/4) (1/6) (3/9) (4/6))
-  , NS "irc-chat" "wezterm start --class irc-chat -- tiny"
+  , NS "irc-chat" (spTerm "irc-chat" "tiny")
       (appName =? "irc-chat")
       (doFloatAt 0.2 0.3)
-  , NS "bottom" "wezterm start --class systemMonitor -- btm"
+  , NS "bottom" (spTerm "systemMonitor" "btm")
       (appName =? "systemMonitor")
       (customFloating $ W.RationalRect (1/6) (1/9) (4/6) (7/9))
-  , NS "calc" "wezterm start --class calc -- kalker"
+  , NS "calc" (spTerm "calc" "kalker")
       (appName =? "calc")
       (customFloating $ W.RationalRect (1/4) (2/6) (1/4) (3/6))
-  , NS "notes" "wezterm start --class notes -- nvim /home/pierre/.local/share/notes.md"
+  , NS "notes" (spTerm "notes" "nvim /home/pierre/.local/share/notes.md")
       (appName =? "notes")
       (doFloatAt 0.2 0.3)
-  , NS "network" "wezterm start --class network -- nmtui"
+  , NS "network" (spTerm "network" "nmtui")
       (appName =? "network")
       (customFloating $ W.RationalRect 0.3 0.14 0.5 0.7)
   , NS "insomnia" "flatpak run rest.insomnia.Insomnia" (className =? "Insomnia")
