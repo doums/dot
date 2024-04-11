ee2ac474f1e694a17d0f9787249dc0168b2464ce
pierre <dommerc.pierre@gmail.com>
2024-04-11 10:26:16 +0200

    update nnn cdonquit on demand only

--- .config/fish/functions/n.fish
+++ .config/fish/functions/n.fish
@@ -1,23 +1,31 @@
-# nnn - cd on quit mode
+# nnn - cd on quit
+# https://github.com/jarun/nnn/blob/master/misc/quitcd/quitcd.fish
 
-function n --wraps nnn --description 'launch nnn with cd on quit mod'
-  # Block nesting of nnn in subshells
-  if test -n "$NNNLVL"
-    if [ (expr $NNNLVL + 0) -ge 1 ]
-      echo "nnn is already running"
-      return
+function n --wraps nnn --description 'support nnn quit and change directory'
+    # Block nesting of nnn in subshells
+    if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
+        echo "nnn is already running"
+        return
     end
-  end
 
-  # nnn will write the cd command in this file on quit
-  set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "-x" from both lines below,
+    # without changing the paths.
+    if test -n "$XDG_CONFIG_HOME"
+        # set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+        set NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    else
+        # set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+        set NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+    end
 
-  # the `command` command allows one to alias this function to
-  # `nnn` without making an infinitely recursive alias
-  command nnn $argv
+    # The command function allows one to alias this function to `nnn` without
+    # making an infinitely recursive alias
+    command nnn $argv
 
-  if test -e $NNN_TMPFILE
-    source $NNN_TMPFILE
-    rm $NNN_TMPFILE
-  end
+    if test -e $NNN_TMPFILE
+        source $NNN_TMPFILE
+        rm -- $NNN_TMPFILE
+    end
 end
