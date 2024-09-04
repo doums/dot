e6237e9862226bb5c86a08e31b2c2496e5784566
pierre <dommerc.pierre@gmail.com>
2024-09-04 22:46:15 +0200

    update wacom tablette script

--- .local/bin/wacom_setup.sh
+++ .local/bin/wacom_setup.sh
@@ -2,7 +2,8 @@
 #
 # Wacom Intuos Pro Medium M PTH-660-N
 
-set -e
+set -E
+set -o pipefail
 
 # get the device id
 stylus_id=$(xsetwacom list devices | rg STYLUS | awk '{print $8}')
@@ -12,7 +13,13 @@ pad_id=$(xsetwacom list devices | rg PAD | awk '{print $8}')
 # tablette active area is 224x148mm ratio → 1.51
 # keep the same physical ratio for the screen area 1800x1189 pixels
 # and offset the screen drawing area to the center of the screen
-xsetwacom set "$stylus_id" MapToOutput 1800x1189+380+1200
+map="1800x1189+380+1200"
+
+# if second monitor is 1080p on left
+if xrandr --listactivemonitors | rg -q '\+DP-\d 1920.*x1080'; then
+  map="1800x1189+2300+1200"
+fi
+xsetwacom set "$stylus_id" MapToOutput $map
 xsetwacom set "$stylus_id" PanScrollThreshold 6000 # default 2600
 
 # set the pad buttons
@@ -25,9 +32,9 @@ xsetwacom set "$stylus_id" PanScrollThreshold 6000 # default 2600
 
 xsetwacom set "$pad_id" Button 1 "key +ctrl z -ctrl" # undo
 xsetwacom set "$pad_id" Button 2 "key +ctrl y -ctrl" # redo
-xsetwacom set "$pad_id" Button 3 "key +alt n" # next color in palette
-xsetwacom set "$pad_id" Button 8 "key +alt b" # prev color in palette
-xsetwacom set "$pad_id" Button 9 "key +super"
+xsetwacom set "$pad_id" Button 3 "key +alt n"        # next color in palette
+xsetwacom set "$pad_id" Button 8 "key +alt b"        # prev color in palette
+xsetwacom set "$pad_id" Button 9 "key +alt j"        # last color in palette
 xsetwacom set "$pad_id" Button 10 "key +ctrl"
 xsetwacom set "$pad_id" Button 11 "key +shift"
 xsetwacom set "$pad_id" Button 9 "key +super"
