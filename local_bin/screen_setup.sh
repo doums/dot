5820fffa99f69d25fdafd1639521348c9c75015f
pierre <dommerc.pierre@gmail.com>
2025-02-03 13:37:02 +0100

    update xmonad and xmobar configs

---
+++ local_bin/screen_setup.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+set -e
+
+# ANSI style codes
+GRN="\e[38;5;2m" # green
+YLW="\e[38;5;3m" # yellow
+MGT="\e[38;5;5m" # magenta
+BLD="\e[1m"      # bold
+ITL="\e[3m"      # italic
+RS="\e[0m"       # style reset
+B_GRN="$BLD$GRN"
+B_YLW="$BLD$YLW"
+B_MGT="$BLD$MGT"
+BI_MGT="$ITL$B_MGT"
+####
+
+xmonad_hs=/opt/xmonad/xmonad.hs
+xconfdir=/etc/X11/xorg.conf.d
+xmobar_cfg="$XDG_CONFIG_HOME"/xmobar/xmobarrc
+rofi_theme="$XDG_CONFIG_HOME"/rofi/theme.rasi
+wezterm_cfg="$XDG_CONFIG_HOME"/wezterm/wezterm.lua
+
+# FHD settings
+declare -A FHD_cfg
+FHD_cfg[screen_conf]=benq_XL2546X
+FHD_cfg[dpi]=144
+FHD_cfg[dmenu_fsize]=26
+FHD_cfg[layout_hcenter]='0.8 1.0'
+FHD_cfg[xmobar_dpi]=120
+FHD_cfg[xmobar_pos]="28 0 140 0 0"
+FHD_cfg[xmobar_font]="JetBrainsMono SZMDI 11"
+FHD_cfg[rofi_yoffset]=200px
+FHD_cfg[term_fontsize]=12.0
+FHD_cfg[tray_h]=28
+FHD_cfg[tray_w]=140
+FHD_cfg[tray_p]=6
+
+# QHD 1440p settings
+declare -A QHD_cfg
+QHD_cfg[screen_conf]=alienware_AW2725DF
+QHD_cfg[dpi]=144
+QHD_cfg[dmenu_fsize]=28
+QHD_cfg[layout_hcenter]='0.8 1.0'
+QHD_cfg[xmobar_dpi]=144
+QHD_cfg[xmobar_pos]="30 0 200 0 0"
+QHD_cfg[xmobar_font]="JetBrainsMono SZMDI 11"
+QHD_cfg[rofi_yoffset]=400px
+QHD_cfg[term_fontsize]=12.0
+QHD_cfg[tray_h]=30
+QHD_cfg[tray_w]=200
+QHD_cfg[tray_p]=8
+
+# 4K settings
+declare -A UHD_cfg
+UHD_cfg[screen_conf]=benq_RD280UA
+UHD_cfg[dpi]=168
+UHD_cfg[dmenu_fsize]=30
+UHD_cfg[layout_hcenter]='0.8 0.9'
+UHD_cfg[xmobar_dpi]=168
+UHD_cfg[xmobar_pos]="40 0 240 0 0"
+UHD_cfg[xmobar_font]="JetBrainsMono SZMDI 12"
+UHD_cfg[rofi_yoffset]=1100px
+UHD_cfg[term_fontsize]=14.0
+UHD_cfg[tray_h]=40
+UHD_cfg[tray_w]=240
+UHD_cfg[tray_p]=10
+
+log() {
+  echo -e "$1"
+}
+
+setup() {
+  local -n cfg=$1
+
+  # switch xorg monitor conf
+  cp $xconfdir/02-"${cfg[screen_conf]}".conf.bak $xconfdir/02-monitor_layout.conf
+  log "  ${B_GRN}switched$RS to $B_YLW${cfg[screen_conf]}.conf$RS"
+
+  # update xmonad config
+  cp $xmonad_hs ${xmonad_hs}.bak
+  sed -Ei "s|^dmenuFnSize =.+|dmenuFnSize = ${cfg[dmenu_fsize]}|" "$xmonad_hs"
+  log "  ${B_GRN}updated$RS dmenu fontsize $B_YLW${cfg[dmenu_fsize]}$RS"
+  sed -Ei "s|hCentered = bottomIfSingle.+|hCentered = bottomIfSingle ${cfg[layout_hcenter]}|" "$xmonad_hs"
+  log "  ${B_GRN}updated$RS layout hCenter $B_YLW${cfg[layout_hcenter]}$RS"
+  ## update systray
+  sed -Ei "s|^trayW =.+|trayW = ${cfg[tray_w]}|" "$xmonad_hs"
+  log "  ${B_GRN}updated$RS system-tray width $B_YLW${cfg[tray_w]}$RS"
+  sed -Ei "s|^trayH =.+|trayH = ${cfg[tray_h]}|" "$xmonad_hs"
+  log "  ${B_GRN}updated$RS system-tray height $B_YLW${cfg[tray_h]}$RS"
+  sed -Ei "s|^trayP =.+|trayP = ${cfg[tray_p]}|" "$xmonad_hs"
+  log "  ${B_GRN}updated$RS system-tray padding $B_YLW${cfg[tray_p]}$RS"
+  delta -s --paging=never ${xmonad_hs}.bak $xmonad_hs || true
+  log
+
+  # update Xresources DPI
+  sed -Ei "s|^Xft.dpi.+|Xft.dpi: ${cfg[dpi]}|" "$HOME"/.Xresources
+  log "  ${B_GRN}updated$RS .Xresources DPI $B_YLW${cfg[dpi]}$RS"
+
+  # update xmobar config
+  cp "$xmobar_cfg" "$xmobar_cfg".bak
+  sed -Ei "s|font = \".+\"|font = \"${cfg[xmobar_font]}\"|" "$xmobar_cfg"
+  log "  ${B_GRN}updated$RS xmobar font $B_YLW${cfg[xmobar_font]}$RS"
+  sed -Ei "s|dpi = .+|dpi = ${cfg[xmobar_dpi]}|" "$xmobar_cfg"
+  log "  ${B_GRN}updated$RS xmobar DPI $B_YLW${cfg[xmobar_dpi]}$RS"
+  sed -Ei "s|position = BottomHM.+|position = BottomHM ${cfg[xmobar_pos]}|" "$xmobar_cfg"
+  log "  ${B_GRN}updated$RS xmobar position $B_YLW${cfg[xmobar_pos]}$RS"
+  delta -s --paging=never "$xmobar_cfg".bak "$xmobar_cfg" || true
+  log
+
+  # update rofi config
+  sed -Ei "s|y-offset: .+;|y-offset: ${cfg[rofi_yoffset]};|" "$rofi_theme"
+  log "  ${B_GRN}updated$RS rofi yoffset $B_YLW${cfg[rofi_yoffset]}$RS"
+
+  # update wezterm config
+  cp "$wezterm_cfg" "$wezterm_cfg".bak
+  sed -Ei "s|^c.dpi = .+|c.dpi = ${cfg[dpi]}|" "$wezterm_cfg"
+  log "  ${B_GRN}updated$RS wezterm DPI $B_YLW${cfg[dpi]}$RS"
+  sed -Ei "s|^c.font_size = .+|c.font_size = ${cfg[term_fontsize]}|" "$wezterm_cfg"
+  log "  ${B_GRN}updated$RS wezterm font size $B_YLW${cfg[term_fontsize]}$RS"
+  delta -s --paging=never "$wezterm_cfg".bak "$wezterm_cfg" || true
+  log
+}
+
+res=$(printf '4K\nQHD\nFHD' | fzf --no-info --header="setup")
+if [ -z "$res" ]; then
+  exit 0
+fi
+
+case $res in
+FHD)
+  setup 'FHD_cfg'
+  ;;
+QHD)
+  setup 'QHD_cfg'
+  ;;
+4K)
+  setup 'UHD_cfg'
+  ;;
+esac
+
+log "$B_GRN✓$RS ${BI_MGT}$res$RS"
+log
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,31 +4,36 @@ import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import Data.Monoid
+import Data.Char
+import XMonad.Actions.OnScreen
 import XMonad.Actions.CycleWS
 import XMonad.Actions.CycleRecentWS
 import XMonad.Actions.Navigation2D
-import Data.Char
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Renamed
 import XMonad.Layout.Spacing
+import XMonad.Layout.CenteredIfSingle
+import XMonad.Layout.Reflect
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.WorkspaceHistory
+import XMonad.Hooks.WorkspaceHistory (workspaceHistoryHook)
 import XMonad.Actions.PerWorkspaceKeys
 import XMonad.Util.Cursor
 import XMonad.Util.SpawnOnce
 import XMonad.Util.NamedScratchpad
 import qualified XMonad.Util.Hacks as Hacks
 import Graphics.X11.ExtraTypes.XF86
+import BottomIfSingle
 
 main :: IO ()
 main = xmonad
      . docks
+     . setEwmhActivateHook doFocus0
      . ewmhFullscreen
      . ewmh
      . withSB (statusBarProp "xmobar" (pure bar))
@@ -40,9 +45,9 @@ cfg = def {
   terminal           = myTerminal,
   focusFollowsMouse  = True,
   clickJustFocuses   = False,
-  borderWidth        = 4,
+  borderWidth        = 6,
   modMask            = modm,
-  workspaces         = myWorkspaces,
+  workspaces         = wspaces,
   normalBorderColor  = darkGrey,
   focusedBorderColor = grey,
   layoutHook         = myLayout,
@@ -54,6 +59,10 @@ cfg = def {
   `additionalKeysP`         keybinds
   `removeKeysP`             kbRemoved
   `additionalMouseBindings` mousebinds
+
+-- custom hook to catch window activation (_NET_ACTIVE_WINDOW) requests
+-- and focus on main screen only
+doFocus0 = ask >>= \w -> doF (onScreen (W.focusWindow w) FocusNew 0)
 
 -- # Keymap
 keybinds = ([
@@ -69,13 +78,14 @@ keybinds = ([
   -- "M-<Workspace key>" Move to workspace x
   -- "M-S-<Workspace key>" Move current window to workspace x
   -- Switch to last workspace
-  , ("M-<Tab>",       toggleRecentWS)
+  -- , ("M-<Tab>",     cycleWorkspaceOnCurrentScreen [xK_Super_L] xK_h xK_l)
+  , ("M-<Tab>",     toggleWindowSets (recentWS ((/= "r") . W.tag)))
   -- Switch to next workspace
-  , ("M-<Page_Up>",   nextWS)
+  , ("M-C-<Right>", nextWS)
   -- Switch to previous workspace
-  , ("M-<Page_Down>", prevWS)
+  , ("M-C-<Left>",  prevWS)
   -- Exec the action of the current workspace
-  , ("M-<Return>",    chooseAction wsActions)
+  , ("M-<Return>",  chooseAction wsActions)
 
   -- ## Window navigation
   -- "M-↑→↓←" Navigate through windows
@@ -114,6 +124,8 @@ keybinds = ([
   , ("M-;",         sendMessage (IncMasterN 1))
   -- Decrement master slots
   , ("M-,",         sendMessage (IncMasterN (-1)))
+  , ("M-C-g",       (toggleScreenSpacingEnabled 
+                      >> toggleWindowSpacingEnabled))
 
   -- ## Launch apps
   -- Open a terminal
@@ -127,9 +139,9 @@ keybinds = ([
   -- Open glyph menu
   , ("M-g",         spawn ("glyph.sh" ++ dmenuArgs))
   -- Open ssh.sh
-  , ("M-S-s",        spawn ("ssh.sh" ++ dmenuArgs))
+  , ("M-S-s",       spawn ("ssh.sh" ++ dmenuArgs))
   -- Open vm.sh
-  , ("M-S-v",        spawn ("vm.sh" ++ dmenuArgs))
+  , ("M-S-v",       spawn ("vm.sh" ++ dmenuArgs))
   -- Open clipboard manager
   , ("M-v",         spawn ("clipmenu -b -i -p '◧'" ++ dmenuArgs))
   -- Restart compositor
@@ -158,10 +170,6 @@ keybinds = ([
   , ("M-i", namedScratchpadAction scratchpads "irc-chat")
   -- System monitor
   , ("M-s", namedScratchpadAction scratchpads "bottom")
-  -- Network Manager
-  , ("M-u", namedScratchpadAction scratchpads "network")
-  -- Insomnia
-  , ("M-S-i", namedScratchpadAction scratchpads "insomnia")
 
   -- ## Multimedia
   -- Light up
@@ -177,14 +185,14 @@ keybinds = ([
   -- Mute mic
   , ("<XF86AudioMicMute>",      spawn "pral.sh source_mute") ]
   ++
-  [ (m ++ "M-" ++ [k], f i)
-      | (i, k) <- zip myWorkspaces workspaceKeys
-      , (f, m) <- [(windows . W.greedyView, ""), (windows . W.shift, "S-")] ]
+  [ (m ++ "M-" ++ [k], windows $ f i)
+      | (i, k) <- zip wspaces wsKeys
+      , (f, m) <- [(viewOnScreen 0, ""), (W.shift, "S-")]
+  ]
   ++
-  [ (m ++ "M-" ++ [k], screenWorkspace sc >>= flip whenJust f)
-      | (k, sc) <- zip screenKeys [0..]
-      , (f, m) <- [(windows . W.view, ""), (windows . W.shift, "S-")] ]
-  )
+  [ ("M-r", windows (viewOnScreen 1 "r"))
+    , ("M-S-r", windows $ W.shift "r")
+  ])
 
 -- ## Mouse bindings
 -- "M-<left click>" Drag the window and make it floating
@@ -205,53 +213,91 @@ kbRemoved = ["M-S-c"]
 
 modm = mod4Mask
 myTerminal = "wezterm"
--- workspaces: 󰞷 󰖟 󱃖 󰭹 z 󰅶 4 r 5
-myWorkspaces = ["\985015", "\984479", "\987350", "\985977", "z", "\983414", "4", "r", "5"]
-workspaceKeys = "&é\"aze'r("
+-- workspaces
+-- 󰞷 󰖟 󱃖 󰭹 z 󰅶 4 r 5
+wspaces = ["\985015", "\984479", "\987350", "\985977", "z", "\983414", "4", "5", "r"]
+wsKeys = "&é\"aze'("
 screenKeys = "[]"
+-- trayer config
+trayW = 240
+trayH = 40
+trayP = 10
 
 grey = "#404040"
 darkGrey = "#262626"
 stone = "#8c8c8c"
 
 -- dmenu config
-dmenuFn = "'Roboto Condensed:pixelsize=22:antialias=true'"
+dmenuFnSize = 30
+dmenuFn = "'Roboto Condensed:pixelsize=" ++ show dmenuFnSize ++ ":antialias=true'"
 dDarkGrey = "\\#262626"
 dNearBlack = "\\#0d0d0d"
 dStone = "\\#8c8c8c"
 dmenuArgs = " -fn " ++ dmenuFn ++ " -nb " ++ dNearBlack ++ " -nf " ++ dStone ++ " -sb " ++ dDarkGrey ++ " -sf " ++ dStone
 
 myLayout = renamed [CutWordsLeft 1]
-           $ smartBorders
+           $ lessBorders Screen
            $ avoidStruts
            $ spacingWithEdge 7
-           $ tiled ||| mirror ||| full
+           $ onWorkspace "r" (stackUpDU ||| full ||| vCenter)
+           $ hCenter ||| stock ||| full ||| stackUp
   where
-    tiled   = renamed [Replace "→"] (Tall nmaster delta ratio)
-    mirror  = renamed [Replace "↓"] (Mirror tiled)
-    full    = renamed [Replace "■"] Full
-    nmaster = 1 -- The default number of windows in the master pane
-    ratio   = 1/2 -- Default proportion of screen occupied by master pane
-    delta   = 3/100 -- Percent of screen to increment by when resizing panes
+    -- horizontally centered with stack on right
+    hCenter   = renamed [Replace "∎"] (hCentered tiledCol)
+    -- default stock layout, master full, stack on right
+    stock     = renamed [Replace "→"] tiledCol
+    -- fullscreen
+    full      = renamed [Replace "■"] Full
+    -- vertically centered, stack on top (DualUp)
+    vCenter   = renamed [Replace "≡"] (vCentered (reflectVert tiledRow))
+    -- stack on top
+    stackUp   = renamed [Replace "↑"] (reflectVert tiledRow)
+    -- DualUp version
+    stackUpDU = renamed [Replace "↑"] (bottom (reflectVert tiledRow))
+    -- bottom stack
+    stackDown = renamed [Replace "↓"] tiledRow
+    -- DualUp version
+    stakDownDU  = renamed [Replace "↓"] (bottom tiledRow)
+    nmaster   = 1 -- The default number of windows in the master pane
+    ratio     = 0.7 -- Default proportion of screen occupied by master pane
+    delta     = 3/100 -- Percent of screen to increment by when resizing panes
+    tiledCol  = Tall nmaster delta ratio
+    tiledRow  = Mirror tiledCol
+    hCentered = bottomIfSingle 0.8 0.9
+    vCentered = centeredIfSingle 1.0 0.7
+    bottom    = bottomIfSingle 1.0 0.7
 
-myManageHook = fmap not willFloat --> insertPosition Below Newer
-               <+> namedScratchpadManageHook scratchpads
-               <+> composeAll
-    [ className =? "Gimp"               --> doFloat
-    , className =? "feh"                --> doFloat
-    , className =? "jetbrains-toolbox"  --> doCenterFloat
-    , className =? "frame"              --> doCenterFloat
-    , title =? "frame"                  --> doCenterFloat
-    , className =? "Gpick"              --> doFloat
+-- use composeOne and `-?>` to avoid floating windows popping
+-- behind (instead of on top) other floating windows
+-- see https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Limit_windows_forced_down_by_using_composeOne
+-- see https://xmonad.github.io/xmonad-docs/xmonad/XMonad-ManageHook.html
+-- for appName className title etc
+myManageHook = namedScratchpadManageHook scratchpads
+               <+> composeOne
+    [ isDialog                          -?> doFloat
+    , className =? "feh"                -?> doFloat
+    , className =? "jetbrains-toolbox"  -?> doCenterFloat
+    , className =? "frame"              -?> doCenterFloat
+    , title =? "frame"                  -?> doCenterFloat
+    , className =? "Gpick"              -?> doFloat
+    , className =? "steam"              -?> doFloat
     , title =? "splash"
-        <&&> className ^? "jetbrains-"  --> doCenterFloat <> hasBorder False
-    , title ^? "Emulator"               --> doFloat
+        <&&> className ^? "jetbrains-"  -?> doCenterFloat <> hasBorder False
+    , title ^? "Emulator"               -?> doFloat
     , (className =? "firefox" <&&> resource =? "Dialog")
-                                        --> doFloat
-    , appName =? "virt-manager"         --> doFloat
-    , appName =? "virt-viewer"          --> doFloat
-    , title     =? "Volume Control"     --> doCenterFloat
-    , resource  =? "desktop_window"     --> doIgnore ]
+                                        -?> doFloat
+    , title     =? "Volume Control"     -?> doCenterFloat
+    , resource  =? "desktop_window"     -?> doIgnore
+    , title ^? "NymVPN"                 -?> doFloat
+    , appName =? "virt-manager"         -?> doFloat
+    , appName =? "virt-viewer"          -?> doFloat
+    , (appName ^? "jetbrains" <&&> title ^? "Commit:")
+                                        -?> doFloat
+    , (role ^? "gimp-toolbox" <||> role =? "gimp-image-window")
+                                        -?> doFloat
+    , return True -?> doF W.swapDown -- shoud be last
+    ]
+    where role = stringProperty "WM_WINDOW_ROLE"
 
 myEventHook = handleEventHook def
                 <> Hacks.trayerAboveXmobarEventHook
@@ -260,6 +306,7 @@ myEventHook = handleEventHook def
 myLogHook = workspaceHistoryHook
 
 myStartupHook = do
+    windows (onlyOnScreen 1 "r")
     setDefaultCursor xC_left_ptr -- set default cursor
     spawnOnce "xwallpaper --daemon --clear --zoom $BG_PRIMARY"
     spawnOnce "picom.sh"
@@ -269,7 +316,11 @@ myStartupHook = do
     -- spawnOnce "bato"
     spawnOnce "solaar -w hide"
     spawnOnce "clipmenud"
-    spawnOnce "trayer -l --align left --distancefrom left --distance 540 --monitor primary --widthtype request --height 28 --transparent true --alpha 0 --tint 0x262626 --expand true --iconspacing 4 --SetPartialStrut true --SetDockType true"
+    spawnOnce ("trayer --monitor primary --transparent true --alpha 120 --tint 0x212121 --iconspacing 4 --SetPartialStrut true --SetDockType true --align right --widthtype pixel" 
+        ++ " --height " ++ show trayH
+        ++ " --width " ++ show trayW 
+        ++ " --padding " ++ show trayP)
+    spawnOnce "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 
 -- xmobar
 bar = def
@@ -300,7 +351,7 @@ wsActions ws = case ws of
   -- 󱃖
   "\987350" -> spawn $ "jetbrains-toolbox"
   -- 󰭹
-  "\985977" -> spawn "im.riot.Riot --profile nym"
+  "\985977" -> spawn "org.zulip.Zulip"
   _         -> undefined
 
 -- helper to define terminal commands for scratchpads
@@ -316,31 +367,26 @@ spTerm className cmd = termCmd
 scratchpads =
   [ NS "translate" (spTerm "translate" "gtt")
       (appName =? "translate")
-      (customFloating $ W.RationalRect 0.25 0.25 0.6 0.4)
-  , NS "pavucontrol" "pavucontrol" (className =? "Pavucontrol")
-      (customFloating $ W.RationalRect (1/4) (1/4) (2/4) (2/4))
+      (customFloating $ W.RationalRect 0.20 0.25 0.5 0.45)
+  , NS "pavucontrol" "pavucontrol" (className =? "pavucontrol")
+      (customFloating $ W.RationalRect 0.3 0.3 0.32 0.38)
   , NS "keymap" "apekey"
       (title =? "apekey")
-      (customFloating $ W.RationalRect 0.38 0.12 0.33 0.8)
+      (customFloating $ W.RationalRect 0.38 0.12 0.33 0.6)
   , NS "file-manager" (spTerm "file-manager" "nnn")
       (appName =? "file-manager")
       (customFloating $ W.RationalRect (1/4) (1/6) (3/9) (4/6))
   , NS "irc-chat" (spTerm "irc-chat" "tiny")
       (appName =? "irc-chat")
-      (doFloatAt 0.2 0.3)
+      (doFloatAt 0.32 0.3)
   , NS "bottom" (spTerm "systemMonitor" "btm")
       (appName =? "systemMonitor")
-      (customFloating $ W.RationalRect (1/6) (1/9) (4/6) (7/9))
+      (customFloating $ W.RationalRect (1/6) 0.25 (4/6) 0.65)
   , NS "calc" (spTerm "calc" "kalker")
       (appName =? "calc")
-      (customFloating $ W.RationalRect (1/4) (2/6) (1/4) (3/6))
+      (customFloating $ W.RationalRect 0.36 (2/6) 0.2 0.3)
   , NS "notes" (spTerm "notes" "nvim /home/pierre/.local/share/notes.md")
       (appName =? "notes")
-      (doFloatAt 0.2 0.3)
-  , NS "network" (spTerm "network" "nmtui")
-      (appName =? "network")
-      (customFloating $ W.RationalRect 0.3 0.14 0.5 0.7)
-  , NS "insomnia" "flatpak run rest.insomnia.Insomnia" (className =? "Insomnia")
-      (customFloating $ W.RationalRect (1/4) (1/4) (2/4) (2/4))
+      (doFloatAt 0.30 0.32)
   ]
 
