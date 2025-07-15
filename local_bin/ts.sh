6085bef3ce252940293088b0312bd59b98b066c9
pierre <dommerc.pierre@gmail.com>
2025-07-15 23:14:50 +0200

    update tailscale script

--- local_bin/ts.sh
+++ local_bin/ts.sh
@@ -2,32 +2,62 @@
 
 # Tailscale up/down wrapper
 
-choices="up (exit linode)\nup\nstatus"
+choices="UP (exit linode)\nUP\nstatus"
 header="tailscale DOWN"
 if tailscale status &>/dev/null; then
-  choices="down\nstatus"
+  choices="exit node UP\nexit node DOWN\naccept dns ON\naccept dns OFF\nallow lan access ON\nallow lan access OFF\nDOWN\nstatus"
   header="tailscale UP"
 fi
+
+status() {
+  tailscale status
+  ip=$(curl -LfsS -4 icanhazip.com)
+  echo -e "\npublic IP: $ip"
+}
 
 choice=$(echo -e "$choices" | fzf --header="$header")
 # if no choice, show status
 if [ -z "$choice" ]; then
-  tailscale status
+  status
   exit 0
 fi
 
 case $choice in
-"up (exit linode)")
-  sudo tailscale up --exit-node=linode --exit-node-allow-lan-access=true
-  tailscale status
+"UP (exit linode)")
+  sudo tailscale up --exit-node=linode --accept-dns --exit-node-allow-lan-access=true
+  status
   ;;
-"up")
-  sudo tailscale up --exit-node="" --exit-node-allow-lan-access=false
-  tailscale status
+"UP")
+  sudo tailscale up --exit-node="" --accept-dns --exit-node-allow-lan-access=false
+  status
   ;;
-"down")
+"exit node UP")
+  sudo tailscale set --exit-node linode
+  status
+  ;;
+"exit node DOWN")
+  sudo tailscale set --exit-node ""
+  status
+  ;;
+"accept dns ON")
+  sudo tailscale set --accept-dns
+  status
+  ;;
+"accept dns OFF")
+  sudo tailscale set --accept-dns=false
+  status
+  ;;
+"allow lan access ON")
+  sudo tailscale set --exit-node-allow-lan-access
+  status
+  ;;
+"allow lan access OFF")
+  sudo tailscale set --exit-node-allow-lan-access=false
+  status
+  ;;
+"DOWN")
   sudo tailscale down
   tailscale status
   ;;
-"status") tailscale status ;;
+"status") status ;;
 esac
